3096a4b2ad61d671fb7bc3869dd325f1e6cf3ee0
Sasa Rkman <sasarkman@u.boisestate.edu>
2020-03-31 19:28:05 -0600

    Add index.html and index.js Modify app.js to bind static content on /index route Add mongodb settings to app.js Add users.json to represent temporary, basic collection Add nodemon developer dependency

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 // Load HTTP module
-// const http = require("http");
 var express = require('express');
+
+// Load logging module
 var logger = require('morgan');
+
+// Load database module
+var db = require('mongodb').MongoClient
+
+// Initialize express and logger
 var app = express();
 app.use(logger('dev'));
 
+// Mongodb connection string
+const mongo_url = "mongodb://127.0.0.1:27017/"
+
+// Express IP and port info
 const hostname = "127.0.0.1";
 const port = 8000;
 
+// db.connect(mongo_url, function(err, database) {
+// 	if (err) throw err;
+// 	var dbo = database.db("local");
+// 	dbo.collection("europe-touch").findOne({}, function(err, result) {
+// 		if (err) throw err;
+// 		console.log("Query: " + JSON.stringify(result));
+// 		database.close();
+// 	});
+// });
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+ 	res.send('Hello World!');
 });
 
+app.use('/index', express.static(__dirname + "/views/index/"));
+
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+ 	console.log('Example app listening on port 3000!');
 });
