67d22d956a607a46ed90db71b67e5e0365a3930a
Sasa Rkman <sasarkman@u.boisestate.edu>
2020-04-06 21:48:02 -0600

    Remove users.json file Remove mongodb dependency since mongoose replaces it Add bcrypt module for password encryption Add express-validator module for user input validation/sanitization Add jsonwebtoken module for web authentication Add mongoose module for mongodb interaction Add account model Implement /login route Implement /createaccount route

--- app.js
+++ app.js
@@ -1,38 +1,103 @@
 // Load HTTP module
-var express = require('express');
+const express = require('express');
+
+// Initialize express
+const app = express();
 
 // Load logging module
-var logger = require('morgan');
+const logger = require('morgan');
 
-// Load database module
-var db = require('mongodb').MongoClient
+// Load object relational model module
+const mongoose = require('mongoose');
 
-// Initialize express and logger
-var app = express();
+// Load authorization module
+const auth = require('jsonwebtoken');
+
+// Load user input validator
+const { check, validationResult } = require('express-validator');
+
+// Load password hashing module
+const bcrypt = require('bcrypt');
+
+// Initialize middlewares
 app.use(logger('dev'));
+app.use(express.urlencoded({extended:true}));
+
+// Define static content delivery
+app.use('/index', express.static(__dirname + "/views/index/"));
 
 // Mongodb connection string
-const mongo_url = "mongodb://127.0.0.1:27017/"
+const mongo_url = "mongodb://127.0.0.1:27017/local"
+
+// Load Models
+var AccountModel = require('./models/account-model');
 
 // Express IP and port info
 const hostname = "127.0.0.1";
 const port = 8000;
 
-// db.connect(mongo_url, function(err, database) {
-// 	if (err) throw err;
-// 	var dbo = database.db("local");
-// 	dbo.collection("europe-touch").findOne({}, function(err, result) {
-// 		if (err) throw err;
-// 		console.log("Query: " + JSON.stringify(result));
-// 		database.close();
-// 	});
-// });
-
-app.get('/', function(req, res) {
- 	res.send('Hello World!');
+mongoose.connect(mongo_url, { useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true }, (err) => {
+	if (err) {
+		console.log("Could not connect to database, exiting...");
+		process.exit(0);
+	}
+	console.log("Connected to database: " + mongo_url);
 });
 
-app.use('/index', express.static(__dirname + "/views/index/"));
+app.post('/login', 
+	[
+		check('email', 'Invalid e-mail address').exists().isEmail(),
+		check('password', 'Password was empty').exists()
+	], function(req, res) {
+		const errors = validationResult(req);
+		if(!errors.isEmpty()) {
+    		return res.status(422).json({ errors: errors.array() });
+		}
+
+		console.log(req.body);
+		var email = req.body.email;
+		var password = req.body.password;
+		
+		// Query database for account with this email
+		AccountModel.findOne({email}, function(err, record) {
+			if(err) res.send(err);
+			else if(record) {
+				record.comparePassword(password, function(err, isMatch) {
+					if(err) res.send(err);
+					if(isMatch) {
+						res.send("Logged in: " + email);
+					} else {
+						res.send("Incorrect password");
+					}
+				});
+			} else {
+				res.send("Account not found: " + email);
+			}
+		})
+
+	});
+
+app.post('/createaccount', 
+	[
+		check('email', 'Invalid e-mail address').exists().isEmail(),
+		check('password', 'Password was empty').exists()
+	], function(req, res) {
+		var email = req.body.email;
+		var password = req.body.password;
+
+		// Query database for how many accounts with this email exist
+		AccountModel.count({email}, function(err, count) {
+			if(err) res.send(err);
+			else if(count > 0) {
+				console.log("Duplicate email: " + email);
+				res.send("duplicate email");
+			} else {
+				var newAccount = new AccountModel({email, password});
+				newAccount.save();
+				res.send("all good!");
+			}
+		})
+	});
 
 app.listen(3000, function() {
  	console.log('Example app listening on port 3000!');
---
+++ models/account-model.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+var Schema = mongoose.Schema;
+
+var AccountSchema = new mongoose.Schema({
+	email: { 
+		type: String, 
+		required: true, 
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
+
+AccountSchema.pre('save', function(next) {
+	var account = this;
+
+	if(!account.isModified('password')) return next();
+
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		if(err) return next("genSalt error");
+
+		bcrypt.hash(account.password, salt, function(err, hash) {
+			if(err) return next("bcrypt hash error");
+
+			account.password = hash;
+			next();
+		})
+	})
+})
+
+AccountSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if(err) return cb(err);
+		cb(null, isMatch);
+	})
+}
+
+module.exports = mongoose.model('accounts', AccountSchema);
+
